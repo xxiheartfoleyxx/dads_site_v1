66e9d23220aaa69a08a91e11b14df3ec49b2c45e
Stefani Kaufman <neffie@Stefanis-MacBook-Pro.local>
2016-07-03 16:39:47 -0400

    still fixing bugs

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
 get '/' do
-  set :public_folder, File.join(APP_ROOT, "Public")
   File.read(File.join("Public", "dads_site2.html"))
+  set :public_folder, File.join(APP_ROOT, "Public")
 end
